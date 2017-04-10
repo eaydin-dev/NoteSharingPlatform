a68752c73e8e3107b2f1507f41e3511be036553f
EA <eyypaydin@gmail.com>
2017-04-10 13:25:06 +0300

    Initial configurations and projecting to RubyMine.

---
+++ app/controllers/materials_controller.rb
@@ -0,0 +1,10 @@
+class MaterialsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ test/controllers/materials_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class MaterialsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get materials_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get materials_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get materials_show_url
+    assert_response :success
+  end
+
+end
