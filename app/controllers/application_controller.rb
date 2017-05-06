8ee9c66989c81f89d8882f14dd4d995c5383c4f3
alieksi <eksi@gtu.edu.tr>
2017-05-06 15:44:38 +0300

    Editing user profile is added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname, :school, :department, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :surname, :school, :department, :email, :password, :password_confirmation, :current_password])
   end
 end
