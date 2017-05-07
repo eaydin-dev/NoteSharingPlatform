73abad11acf57a8b5249132483c852557192bf60
alieksi <eksi@gtu.edu.tr>
2017-05-07 16:50:25 +0300

    flv and mkv are deleted from content type validations.

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,7 +1,7 @@
 class Material < ApplicationRecord
 
 	has_attached_file :data
-	validates_attachment :data, content_type: { content_type: ["application/pdf", "video/mp4", "video/avi", "video/mkv", "video/flv"] }
+	validates_attachment :data, content_type: { content_type: ["application/pdf", "video/mp4", "video/avi"] }
 
 	def self.search(search)
 		where("title LIKE ? OR description LIKE ?", "%#{search}%", "%#{search}%") 
